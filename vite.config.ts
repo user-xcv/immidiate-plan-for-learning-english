dc298878fcc1a3bf6506622e7227b9e8faa18a84
user-xcv <knyazknyaz183@gmail.com>
2026-05-22 21:05:19 +0500

    fixed the bugg

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-    },
+
+
+
+
+
+
+   
   }
 }
 
@@ -26,25 +26,19 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+ 
 
   // Fayllarni build jarayonida to'g'ri asset papkasiga joylashini ta'minlash
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        // Asset fayllar nomini toza saqlash (MIME xatosini oldini olish uchun)
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
+build: {
+  assetsDir: 'assets',
+  rollupOptions: {
+    output: {
+      assetFileNames: 'assets/[name]-[hash][extname]',
+      chunkFileNames: 'assets/[name]-[hash].js',
+      entryFileNames: 'assets/[name]-[hash].js',
     },
   },
+},
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
